b7f218b6ea72e8d7245b7b3c614296dc00b6af25
Alexey Elyakin <ieliakin98@mail.ru>
2023-11-13 20:36:57 +0300

    commit_str_funk2.0

--- str_func.py
+++ str_func.py
@@ -1,4 +1,14 @@
 def str_func(word):
+    """Функция изменения регистра"""
     result = word.upper()
     return result
 
+
+def to_title(word):
+    """Функция изменения первых букв слова к верхнему регистру"""
+    result = word.split(" ")
+    result = [i.title() for i in result]
+    return " ".join(result)
+
+
+print(to_title("jon konor"))
